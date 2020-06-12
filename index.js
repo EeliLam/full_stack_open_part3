d511a8a87e523d579f3ccf1b07d3bba35500d049
EeliLam <eeli.lamponen@aalto.fi>
2020-06-12 23:56:57 +0300

    Puhelinluettelo backend first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+morgan.token('req_body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+const morganFormat = 
+    ':method :url :status :res[content-length] - :response-time ms :req_body'
+
+app.use(morgan(morganFormat))
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    }
+]
+
+app.get('/info', (req, res) => {
+    const response = 
+        `<div>
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${(new Date()).toString()}</p>
+        </div>`
+    res.send(response)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({ error: 'name missing' })
+    }
+    if (!body.number) {
+        return res.status(400).json({ error: 'number missing' })
+    }
+    if (persons.map(p => p.name).includes(body.name)) {
+        return res.status(400).json({ error: 'name must be unique' })
+    }
+
+    const id = Math.round(Math.random() * 1000000)
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: id
+    }
+    
+    person.id = id
+    
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
