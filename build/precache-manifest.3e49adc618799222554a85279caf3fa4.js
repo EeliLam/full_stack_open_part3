8519609dd5be81f194707d2e812e3bdc492dd429
EeliLam <eeli.lamponen@aalto.fi>
2020-06-13 13:11:32 +0300

    3.11 frontend to Heroku

---
+++ build/precache-manifest.3e49adc618799222554a85279caf3fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913644704338b8bea344ce5d024287cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1829636d02a55f4106eb",
+    "url": "/static/css/main.134e9fdd.chunk.css"
+  },
+  {
+    "revision": "3cecff66644f73fc80c9",
+    "url": "/static/js/2.357f7ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.357f7ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1829636d02a55f4106eb",
+    "url": "/static/js/main.07a2db2b.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
